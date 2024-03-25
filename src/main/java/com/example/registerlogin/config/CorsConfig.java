5f9394411a3c58a97db7af2a2bb1aa07dcb63d7f
cheikh-yasmine <161475495+cheikh-yasmine@users.noreply.github.com>
2024-03-26 00:51:21 +0100

    first commit

---
+++ src/main/java/com/example/registerlogin/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.example.registerlogin.config;
+
+import jakarta.servlet.Filter;
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+public class CorsConfig {
+    @Bean
+    public FilterRegistrationBean<Filter> corsFilter() {
+        FilterRegistrationBean<Filter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new CorsFilter());
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(1); // set precedence
+        return registrationBean;
+    }
+}
--- src/main/java/com/example/registerlogin/controller/FileUploadController.java
+++ src/main/java/com/example/registerlogin/controller/FileUploadController.java
@@ -16,7 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/files")
 public class FileUploadController {
 
