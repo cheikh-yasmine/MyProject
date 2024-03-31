71663017e0087c968a1bb56fd1eb4d004d423d78
cheikh-yasmine <161475495+cheikh-yasmine@users.noreply.github.com>
2024-03-31 02:05:09 +0100

    commit

---
+++ src/main/java/com/example/registerlogin/MailSend/MailSenderController.java
@@ -0,0 +1,27 @@
+package com.example.registerlogin.MailSend;
+
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+
+    @RestController
+    @RequestMapping("/mail")
+    public class MailSenderController {
+        private MailService.EmailService emailService;
+
+        public MailSenderController(MailService.EmailService emailService) {
+            this.emailService = emailService;
+        }
+
+        @PostMapping("/send")
+        public String sendMail(@RequestParam(value = "file", required = false) MultipartFile[] file, String to, String[] cc, String subject, String body) {
+            return emailService.sendMail(file, to, cc, subject, body);
+        }
+
+    }
+
